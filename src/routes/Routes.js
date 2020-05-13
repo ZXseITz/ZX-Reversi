0be318165e5f7077478c7a6201db040539810b24
Claudio Seitz <zxseitz@gmail.com>
2020-05-13 19:41:08 +0200

    spelling fix

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -10,11 +10,11 @@ module.exports = (app, db) => {
     app.use(authRouter.path, authRouter.init(express.Router(), db));
     app.use(apiRouter.path, (req, res, next) => {
         const header = req.header('authorization')
-        const bearar = bearerRegex.exec(header);
-        if (!bearar) {
+        const bearer = bearerRegex.exec(header);
+        if (!bearer) {
             res.status(401).send("Invalid auth header");
         } else {
-            const jwt = bearar[1];
+            const jwt = bearer[1];
             verifyJWT(jwt, (err, user) => {
                 if (err) {
                     res.status(401).send("Invalid jwt");
