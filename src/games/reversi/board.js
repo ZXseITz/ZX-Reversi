5c574ea74f65d731d7a25c4ec7ceac0e36341a65
Claudio <zxseitz@gmail.com>
2020-10-21 15:23:05 +0200

    Implemented action validation and list affected opponent tokens

--- src/games/reversi/board.js
+++ src/games/reversi/board.js
@@ -1,6 +1,13 @@
 /**
  * Possible game states
- * @type {{NEXT_WHITE: number, END_BLACK_VICTORY: number, NEXT_BLACK: number, END_WHITE_VICTORY: number, END_TIE: number}}
+ *
+ * @type {{
+ *   NEXT_WHITE: number,
+ *   END_BLACK_VICTORY: number,
+ *   NEXT_BLACK: number,
+ *   END_WHITE_VICTORY: number,
+ *   END_TIE: number
+ * }}
  */
 const States = {
     NEXT_WHITE: 1,
@@ -12,7 +19,14 @@ const States = {
 
 /**
  * Possible error codes
- * @type {{GAME_FINISHED: number, INVALID_COORDINATE: number, NO_ERROR: number, NOT_YOUR_GAME: number, NOT_YOUR_TURN: number}}
+ *
+ * @type {{
+ *   GAME_FINISHED: number,
+ *   INVALID_COORDINATE: number,
+ *   NO_ERROR: number,
+ *   NOT_YOUR_GAME: number,
+ *   NOT_YOUR_TURN: number
+ * }}
  */
 const ErrorCodes = {
     NO_ERROR: 0,
@@ -23,11 +37,41 @@ const ErrorCodes = {
 }
 
 /**
+ * Directions iterators
+ *
+ * @type {{
+ *   DOWN: {next: (function(number): number), check: (function(number): boolean)},
+ *   LEFT: {next: (function(number): number), check: (function(number): boolean)},
+ *   RIGHT: {next: (function(number): number), check: (function(number): boolean)},
+ *   UP: {next: (function(number): number), check: (function(number): boolean)},
+ * }}
+ */
+const directions = {
+    LEFT: {
+        next: (x) => x - 1,
+        check: (x) => (x % 8) < 7
+    },
+    UP: {
+        next: (x) => x - 8,
+        check: (x) => x >= 0
+    },
+    DOWN: {
+        next: (x) => x + 8,
+        check: (x) => x < 64
+    },
+    RIGHT: {
+        next: (x) => x + 1,
+        check: (x) => (x % 8) > 0
+    },
+}
+
+/**
  * Board class
  */
 class Board {
     /**
      * Class constructor
+     *
      * @param {ObjectID} id - Board ID
      * @param {ObjectID} white - White player ID
      * @param {ObjectID} black - Black player ID
@@ -52,24 +96,49 @@ class Board {
     }
 
     /**
+     * Returns the color of a given player
      *
      * @param {ObjectID} player - Player ID
-     * @return {number} - Returns player color
-     *   0 undefined
-     *   1 white
-     *   2 black
+     * @return {number} - Returns player color: 1 -> white, 2 -> black,
+     * or undefined if the player is not a member of this board
      */
     getColor(player) {
         return player === this.white ? 1 : player === this.black ? 2 : undefined;
     }
 
     /**
+     * List all opponent fields of a given direction iterator
+     *
+     * @param {number} color - Player color: 1 white, 2 black
+     * @param {number} index - Player token index [0, 63]
+     * @param {{
+     *   next: (function(number): number),
+     *   check: (function(number): boolean)
+     * }} direction - direction iterator
+     * @return {[number]} Opponent fields
+     */
+    getOpponentFields(color, index, direction) {
+        const fields = [];
+        const opponentColor = 3 - color;
+        let i = direction.next(index);
+        while (direction.check(i) && this.fields[i] === opponentColor) {
+            fields.push(i);
+            i = direction.next(i);
+        }
+        if (direction.check(i) && this.fields[i] === color) {
+            return fields;
+        }
+        return [];
+    }
+
+    /**
      * Sets a token on this board
+     *
      * @param {ObjectID} player - Player ID
-     * @param {number} coordinate - Token coordinate [0, 63]
+     * @param {number} index - Token coordinate (0, 0) to (7, 7)
      * @return {number} Error code
      **/
-    set(player, coordinate) {
+    set(player, index) {
         // check board state
         if (this.state >= 10) {
             return ErrorCodes.GAME_FINISHED;
@@ -81,23 +150,19 @@ class Board {
         } else if (color !== this.state) {
             return ErrorCodes.NOT_YOUR_TURN;
         }
-        const opponentColor = 3 - color;
         // check field coordinate and range
-        if (coordinate >= 0 && coordinate < 64 && this.fields[coordinate] === 0) {
-            // check neighbours
-            const left = coordinate - 1;
-            const up = coordinate - 8;
-            const right = coordinate + 1;
-            const down = coordinate + 8;
-            if (left > 0 && this.fields[left] === opponentColor ||
-                up > 0 && this.fields[up] === opponentColor ||
-                right > 0 && this.fields[right] === opponentColor ||
-                down > 0 && this.fields[down] === opponentColor) {
-                // correct action
+        if (index >= 0 && index < 64 && this.fields[index] === 0) {
+            const fields = [
+                ...this.getOpponentFields(color, index, directions.LEFT),
+                ...this.getOpponentFields(color, index, directions.UP),
+                ...this.getOpponentFields(color, index, directions.DOWN),
+                ...this.getOpponentFields(color, index, directions.RIGHT)
+            ];
+            if (fields.length > 0) {
+                // valid action
                 // todo perform action
 
                 this.state = States.NEXT_WHITE;
-
                 return ErrorCodes.NO_ERROR;
             }
         }
@@ -107,6 +172,7 @@ class Board {
 
 module.exports = {
     Board,
+    directions,
     States,
     ErrorCodes
 }
--- test/games/reversi/board.test.js
+++ test/games/reversi/board.test.js
@@ -1,5 +1,63 @@
 const {describe, test} = require("@jest/globals")
-const {Board, States, ErrorCodes} = require("../../../src/games/reversi/board.js");
+const {Board, directions, States, ErrorCodes} = require("../../../src/games/reversi/board.js");
+
+
+/**
+  0  1  2  3  4  5  6  7
+  8  9 10 11 12 13 14 15
+ 16 17 18 19 20 21 22 23
+ 24 25 26 27 28 29 30 31
+ 32
+ 40
+ 48 49 50 51 52 53 54 55
+ 56 57 58 59 60 61 62 63
+**/
+
+describe('test left direction iterator', () => {
+    test('next', () => {
+        expect(directions.LEFT.next(20)).toEqual(19);
+    });
+    test('check', () => {
+        expect(directions.LEFT.check(20)).toBeTruthy();
+        expect(directions.LEFT.check(16)).toBeTruthy();
+        expect(directions.LEFT.check(15)).toBeFalsy();
+    });
+});
+
+describe('test up direction iterator', () => {
+    test('next', () => {
+        expect(directions.UP.next(27)).toEqual(19);
+    });
+    test('check', () => {
+        expect(directions.UP.check(27)).toBeTruthy();
+        expect(directions.UP.check(3)).toBeTruthy();
+        expect(directions.UP.check(0)).toBeTruthy();
+        expect(directions.UP.check(-1)).toBeFalsy();
+    });
+});
+
+describe('test down direction iterator', () => {
+    test('next', () => {
+        expect(directions.DOWN.next(19)).toEqual(27);
+    });
+    test('check', () => {
+        expect(directions.DOWN.check(19)).toBeTruthy();
+        expect(directions.DOWN.check(59)).toBeTruthy();
+        expect(directions.DOWN.check(63)).toBeTruthy();
+        expect(directions.DOWN.check(64)).toBeFalsy();
+    });
+});
+
+describe('test right direction iterator', () => {
+    test('next', () => {
+        expect(directions.RIGHT.next(19)).toEqual(20);
+    });
+    test('check', () => {
+        expect(directions.RIGHT.check(19)).toBeTruthy();
+        expect(directions.RIGHT.check(23)).toBeTruthy();
+        expect(directions.RIGHT.check(24)).toBeFalsy();
+    });
+});
 
 describe('test board constructor', () => {
     test('default', () => {
@@ -53,5 +111,42 @@ describe('test set', () => {
     test('valid coordinate', () => {
         expect(board.set(black, 26)).toEqual(ErrorCodes.NO_ERROR);
         expect(board.state).toEqual(States.NEXT_WHITE);
+    });
+});
+
+describe('test get opponent fields', () => {
+    const white = 12;
+    const black = 15;
+    const color = 2;
+    const board = new Board(7, white, black);
+    board.fields = [
+        0, 0, 0, 1, 1, 2, 0, 0,
+        0, 0, 0, 1, 1, 1, 1, 2,
+        0, 0, 0, 1, 1, 1, 1, 0,
+        0, 0, 0, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+    ];
+    test('middle', () => {
+        const fields = board.getOpponentFields(color, 2, directions.RIGHT);
+        expect(fields).toEqual([3, 4]);
+    });
+    test('edge', () => {
+        const fields = board.getOpponentFields(color, 10, directions.RIGHT);
+        expect(fields).toEqual([11, 12, 13, 14]);
+    });
+    test('middle empty', () => {
+        const fields = board.getOpponentFields(color, 18, directions.RIGHT);
+        expect(fields).toEqual([]);
+    });
+    test('edge empty', () => {
+        const fields = board.getOpponentFields(color, 26, directions.RIGHT);
+        expect(fields).toEqual([]);
+    });
+    test('empty', () => {
+        const fields = board.getOpponentFields(color, 34, directions.RIGHT);
+        expect(fields).toEqual([]);
     });
 });
